7730e7cee08a82a8e9d4e2df2fd61651809e4fbe
robin <robinnagpal.tiet@gmail.com>
2023-04-10 13:40:47 -0400

    remove the extra files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import type { AppProps } from "next/app";
 import { FpjsProvider } from "@fingerprintjs/fingerprintjs-pro-react";
 import { configureAbly } from "@ably-labs/react-hooks";
-import styles from "../styles/chat-ui-kit-styles/dist/default/styles.min.css";
-// import protocolConfig from "protocol-config";
 
 const prefix = process.env.API_ROOT || "";
 
@@ -18,17 +16,7 @@ configureAbly({
 const fpjsPublicApiKey = process.env.FINGERPRINT as string;
 
 export default function App({ Component, pageProps }: AppProps) {
-    // const clientName = 'uniswap';
-    // const themePath = protocolConfig[clientName].theme;
-    const themes = [
-        'aave',
-        'compound',
-        'uniswap',
-        'unstoppable',
-    ];
   return (
-    <main className={styles.main}>
-        {/* <link rel="stylesheet" href={themePath} /> */}
         <FpjsProvider
             loadOptions={{
                 apiKey: fpjsPublicApiKey,
@@ -36,7 +24,5 @@ export default function App({ Component, pageProps }: AppProps) {
             >
             <Component {...pageProps} />
         </FpjsProvider>
-    </main>
-    
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,7 +119,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main >
+      <main className={'aave'}>
         <div
           style={{ position: "relative", height: "98vh", overflow: "hidden" }}
         >
